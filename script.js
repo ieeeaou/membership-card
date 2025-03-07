ae93e69f12493ee6188529aa0e7d1b8f94b7a64d
ieeeaou <ieeeaou@gmail.com>
2025-03-07 16:25:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value.trim();
   
-    const validUsername = '1234';
-    const validPassword = '1234';
+    const validUsername = 'ieeeaou';
+    const validPassword = 'ieeeaou';
   
     if (username === validUsername && password === validPassword) {
         // Set session flag to indicate successful login
